5c330a296ce235b61084a99687ef0e65dda0bc0a
ykarpa <yurii.karpa@lnu.edu.ua>
2023-05-28 01:16:01 +0300

    add GameForm, some events, buttons, methods; add class Circle

---
+++ GameCircles/GameForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace GameCircles
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.missedLabel = new System.Windows.Forms.Label();
+            this.recordLabel = new System.Windows.Forms.Label();
+            this.game_Panel = new System.Windows.Forms.Panel();
+            this.ballTimer = new System.Windows.Forms.Timer(this.components);
+            this.stopButton = new System.Windows.Forms.Button();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            resources.ApplyResources(this.scoreLabel, "scoreLabel");
+            this.scoreLabel.Name = "scoreLabel";
+            // 
+            // missedLabel
+            // 
+            resources.ApplyResources(this.missedLabel, "missedLabel");
+            this.missedLabel.Name = "missedLabel";
+            // 
+            // recordLabel
+            // 
+            resources.ApplyResources(this.recordLabel, "recordLabel");
+            this.recordLabel.Name = "recordLabel";
+            // 
+            // game_Panel
+            // 
+            resources.ApplyResources(this.game_Panel, "game_Panel");
+            this.game_Panel.Name = "game_Panel";
+            // 
+            // ballTimer
+            // 
+            this.ballTimer.Interval = 1000;
+            this.ballTimer.Tick += new System.EventHandler(this.ballTimer_Tick);
+            // 
+            // StopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.stopButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlDark;
+            this.stopButton.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.stopButton, "stopButton");
+            this.stopButton.Name = "stopButton";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // continueButton
+            // 
+            this.continueButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.continueButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlDark;
+            this.continueButton.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.continueButton, "continueButton");
+            this.continueButton.Name = "continueButton";
+            this.continueButton.UseVisualStyleBackColor = false;
+            this.continueButton.Click += new System.EventHandler(this.ContinueButton_Click);
+            // 
+            // GameForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.game_Panel);
+            this.Controls.Add(this.recordLabel);
+            this.Controls.Add(this.missedLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GameForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label scoreLabel;
+        private Label missedLabel;
+        private Label recordLabel;
+        private Panel game_Panel;
+        private System.Windows.Forms.Timer ballTimer;
+        private Button stopButton;
+        private Button continueButton;
+    }
+}
---
+++ GameCircles/GameForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Reflection;
+using Timer = System.Windows.Forms.Timer;
+
+namespace GameCircles
+{
+    public partial class GameForm : Form
+    {
+        private Timer removeTimer;
+        private Random random = new Random();
+        private int score = 0;
+        private int maxScore = 0;
+        private int missedBalls = 0;
+        private DateTime startTime;
+
+        public GameForm()
+        {
+            InitializeComponent();
+            this.Load += GameForm_Load;
+            continueButton.Enabled = false;
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        { 
+            ballTimer.Start();
+        }
+
+        private void ballTimer_Tick(object sender, EventArgs e)
+        {
+            Circle circle = new Circle();
+            circle.Size = new Size(50, 50);
+            circle.BackColor = GetRandomColor();
+            circle.Location = GetRandomLocation();
+            circle.Click += circle_Click;
+
+            game_Panel.Controls.Add(circle);
+
+            removeTimer = new Timer();
+            removeTimer.Interval = 1500;
+            removeTimer.Tick += (sender, e) => remove(sender, circle);
+            removeTimer.Start();
+        }
+
+        private void remove(object sender,  Circle circle)
+        {
+            (sender as Timer).Stop();
+            (sender as Timer).Dispose();
+
+            if (game_Panel.Controls.Contains(circle)) {
+                if (circle.BackColor != Color.Green)
+                {
+                    missedBalls++;
+                    UpdateMissedLabel();
+                }
+
+                if (missedBalls >= 5)
+                {
+                    EndGame();
+                }
+
+                game_Panel.Controls.Remove(circle);
+            }
+        }
+
+        private Color GetRandomColor()
+        {
+            Color[] colors = { Color.Red, Color.Green, Color.Blue, Color.Yellow};
+            return colors[random.Next(colors.Length)];
+        }
+
+        private Point GetRandomLocation()
+        {
+            int x = random.Next(0, game_Panel.ClientSize.Width - 50);
+            int y = random.Next(0, game_Panel.ClientSize.Height - 50);
+            return new Point(x, y);
+        }
+
+        private void circle_Click(object sender, EventArgs e)
+        {
+            Circle circle = (Circle)sender;
+            game_Panel.Controls.Remove(circle);
+            ballTimer.Interval = (ballTimer.Interval <= 300) ? 300 : ballTimer.Interval - 20;
+
+            if (circle.BackColor == Color.Yellow)
+                score += 5;
+            if (circle.BackColor == Color.Green)
+                score -= 10;
+            if (circle.BackColor == Color.Blue)
+                score += 1;
+            if (circle.BackColor == Color.Red)
+                score += 10;
+
+            Record(score);
+            UpdateScoreLabel();
+
+            if (score < 0) 
+            {
+                EndGame();
+            }
+        }
+
+        private void Record(int score)
+        {
+            if (score > maxScore)
+                maxScore = score;
+            UpdateRecordLabel();
+        }
+
+        private void UpdateScoreLabel()
+        {
+            scoreLabel.Text = "Кількість очків: " + score.ToString();
+        }
+
+        private void UpdateMissedLabel()
+        {
+            missedLabel.Text = "Пропущені: " + missedBalls.ToString();
+        }
+
+        private void UpdateRecordLabel()
+        {
+            recordLabel.Text = "Найкращий результат: " + maxScore.ToString();
+        }
+
+        private void EndGame()
+        {
+            ballTimer.Enabled = false;
+            removeTimer.Enabled = false;
+            
+            MessageBox.Show("Ваш найкращий результат: " + maxScore.ToString(), "Гра завершена");
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            ballTimer.Enabled = false;
+            removeTimer.Enabled = false;
+            game_Panel.Enabled = false;
+            continueButton.Enabled = true;
+            stopButton.Enabled = false;
+        }
+
+        private void ContinueButton_Click(object sender, EventArgs e)
+        {
+            ballTimer.Enabled = true;
+            removeTimer.Enabled = true;
+            game_Panel.Enabled = true;
+            continueButton.Enabled = false;
+            stopButton.Enabled = true;
+        }
+    }
+}
